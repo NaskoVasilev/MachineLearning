3a91f43b5d5086c3a5dd5aca52780c184abf56a3
NaskoVasilev <nasko_vasilev@abv.bg>
2019-07-29 20:33:38 +0300

    Add JudgeSysteLessonsRecomendation project and input and output models for ml algorithm

---
+++ JudgeSystemLessonsRecomendation/JudgeSystemLessonsRecomendation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JudgeSystemLessonsRecomendation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ JudgeSystemLessonsRecomendation/JudgeSystemLessonsRecomendation/UserLesson.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.ML.Data;
+
+namespace JudgeSystemLessonsRecomendation
+{
+    public class UserLesson
+    {
+        [LoadColumn(0)]
+        public string UserId { get; set; }
+
+        [LoadColumn(1)]
+        public int LessonId { get; set; }
+
+        //[LoadColumn(2)]
+        //public float Lebel { get; set; }
+    }
+}
